b7f1b17254263e42c75885878f4a2098ff9e1d2a
Yuliia Mustafina <mystafinayulija@gmail.com>
2021-03-12 01:29:16 +0200

    feat: add function min max avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === [] || array === undefined || array.length === 0) {
+    return 0;
+  } 
+  let min = Math.min.apply(null, array);
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === [] || array === undefined || array.length === 0) {
+    return 0;
+  }
+  let max = Math.max.apply(null, array);
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === [] || array === undefined || array.length === 0) {
+    return 0;
+  }
+  let total = 0;
+  for(let i = 0; i < array.length; i++) {
+    total += array[i];
+  }
+  let avg = total/array.length;
+  return avg;
 }
